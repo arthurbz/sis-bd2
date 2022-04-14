8116482a126afae0b6f8e4cc48f02ab7ef870703
arthurbz <arthurbziero@gmail.com>
2022-04-14 19:38:09 -0300

    add tabela autor

--- scripts/create_tables.sql
+++ scripts/create_tables.sql
@@ -1,18 +1,25 @@
 USE db_app_db2;
 
 /*CREATE TABLE*/
+CREATE TABLE autor (
+  id_autor BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  autor_nome VARCHAR(100),
+  data_nascimento DATE
+);
+
 CREATE TABLE livro (
   id_livro BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   titulo VARCHAR(100),
-  autor VARCHAR(100),
-  classificacao VARCHAR(100),
+  tema VARCHAR(100),
   paginas INT,
-  data_publicacao DATE
+  data_publicacao DATE,
+  fk_autor BIGINT,
+  CONSTRAINT fk_livro_autor FOREIGN KEY(fk_autor) REFERENCES autor(id_autor)
 );
 
 CREATE TABLE leitor (
   id_leitor BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  nome VARCHAR(100),
+  leitor_nome VARCHAR(100),
   data_nascimento DATE,
   sexo CHAR,
   CONSTRAINT check_sexo CHECK (sexo = 'M' OR sexo = 'F')
@@ -26,8 +33,8 @@ CREATE TABLE leitura (
   classificacao INT,
   fk_leitor BIGINT NOT NULL,
   fk_livro BIGINT NOT NULL,
-  CONSTRAINT fk_leitor FOREIGN KEY(fk_leitor) REFERENCES leitor(id_leitor),
-  CONSTRAINT fk_livro FOREIGN KEY(fk_livro) REFERENCES livro(id_livro),
+  CONSTRAINT fk_leitura_leitor FOREIGN KEY(fk_leitor) REFERENCES leitor(id_leitor),
+  CONSTRAINT fk_leitura_livro FOREIGN KEY(fk_livro) REFERENCES livro(id_livro),
   CONSTRAINT check_classificacao CHECK (classificacao >= 0 AND classificacao <= 10)
 );
 
@@ -35,21 +42,28 @@ CREATE TABLE amizade (
   fk_leitor1 BIGINT NOT NULL,
   fk_leitor2 BIGINT NOT NULL,
   PRIMARY KEY (fk_leitor1, fk_leitor2),
-  CONSTRAINT fk_leitor1 FOREIGN KEY(fk_leitor1) REFERENCES leitor(id_leitor),
-  CONSTRAINT fk_leitor2 FOREIGN KEY(fk_leitor2) REFERENCES leitor(id_leitor)
+  CONSTRAINT fk_amizade_leitor1 FOREIGN KEY(fk_leitor1) REFERENCES leitor(id_leitor),
+  CONSTRAINT fk_amizade_leitor2 FOREIGN KEY(fk_leitor2) REFERENCES leitor(id_leitor)
 );
 
 
 
 /*INSERT*/
-INSERT INTO livro (titulo, autor, classificacao, paginas, data_publicacao)
+INSERT INTO autor (autor_nome, data_nascimento)
 VALUES
-('A República', 'Platão', 'Filosofia', 365, '2021-12-31'),
-('Sobre a brevidade da vida', 'Sêneca', 'Filosofia', 120, '2021-12-31'),
-('Em busca de sentido', 'Vitor E. Frankl', 'Filosofia', 200, '2021-12-31'),
-('Sistemas Operacionais Modernos', 'Andrew Tanenbaum', 'Computadores', 1000, '2021-12-31');
+('Platão', '2000-01-01'),
+('Sêneca', '2000-01-01'),
+('Vitor E. Frankl', '2000-01-01'),
+('Andrew Tanenbaum', '2000-01-01');
 
-INSERT INTO leitor (nome, data_nascimento, sexo)
+INSERT INTO livro (titulo, fk_autor, tema, paginas, data_publicacao)
+VALUES
+('A República', 1, 'Filosofia', 365, '2021-12-31'),
+('Sobre a brevidade da vida', 2, 'Filosofia', 120, '2021-12-31'),
+('Em busca de sentido', 3, 'Filosofia', 200, '2021-12-31'),
+('Sistemas Operacionais Modernos', 4, 'Computadores', 1000, '2021-12-31');
+
+INSERT INTO leitor (leitor_nome, data_nascimento, sexo)
 VALUES
 ('Guilherme Giordani', '2000-10-08', 'M'),
 ('Leonardo Bortolini', '2000-05-10', 'M'),
